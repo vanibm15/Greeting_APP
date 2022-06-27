20d82f71220dbc05d10e673fda015ac08829e0a2
vanibm15 <vanibm15@gmail.com>
2022-06-27 13:49:58 +0530

    added:greeting using html

---
+++ src/main/java/com/bridgelabz/greetingapp/GreetingapppApplication.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.greetingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GreetingapppApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingapppApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/bridgelabz/greetingapp/model/Greeting.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.greetingapp.model;
+
+public class Greeting {
+
+        private final long id;
+        private final String message;
+        public Greeting(long id, String message) {
+            this.id =id;
+            this.message = message;
+        }
+
+
+        public long getId() {
+            return id;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+
